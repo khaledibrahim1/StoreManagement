75a7bca91acece2273db9d3e0218539da71c90d5
khaled <knowjkg@gmail.com>
2026-05-26 17:50:55 +0300

    readme

--- obj/Debug/net8.0-windows/StoreManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/StoreManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+618e8a688d5fea9836ec3d485fa8f7fb887a2d1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
